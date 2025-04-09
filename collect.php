afc6b70f766d39032ccf427fd597273364d4edca
Extranjero <arivera18@rockets.utoledo.edu>
2025-04-09 17:46:44 -0400

    collect-added

---
+++ collect.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstname"]; ?><br>
+Your Last Name is: <?php echo $_POST["lastname"]; ?>
+Your age is: <?php echo $_POST["age"];?>
+The photo that you submitted is here: <?php echo $_POST["photo"];?>
+Your current satisfaction level is: <?php echo $_POST["satisfaction"];?>
+Your favorite color is: <?php echo $_POST["favorite"];?>
+</body>
+</html>
